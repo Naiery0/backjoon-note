76f05f784abe17e05e7de4049718bb5a0e9d4e23
Naiery0 <molly724@naver.com>
2025-06-16 17:30:48 +0900

    5푸는중

---
+++ C99_Step_by_Step/Step05/10809-알파벳찾기.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+/*
+* in
+*   첫째 줄에 단어 S 소문자로만
+*   baekjoon
+* out
+*   각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를 공백으로 구분
+*   1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+*/
+
+int main() {
+    // printf("%d", 'a'); //97
+    char S[100];
+    scanf("%s", S);
+
+    for (int i = 0; i < 26; i++) {
+
+    }
+    
+    return 0;
+}
--- C99_Step_by_Step/Step05/11720-숫자합.c
+++ C99_Step_by_Step/Step05/11720-숫자합.c
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+//#include <stdlib.h>
+
 /*
 * in
 *   첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100). 
@@ -15,6 +17,16 @@ int main() {
     int N, sum = 0;
     scanf("%d", &N);
 
+    char st[100];
+    scanf("%s", st);
+
+    for (int i = 0; i < N; i++)
+    {
+        //sum += atoi(st[i]); - 컴파일 에러 문자열이 아니라 문자를 변환하려고 해서
+        sum += st[i] - '0'; // - 문자 0을 빼면 숫자가 된다
+    }
+
+    printf("%d", sum);
 
     return 0;
 }
