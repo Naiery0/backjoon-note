2ad3861f58c12fc366b0b74f997adbd040c27cde
Naiery0 <molly724@naver.com>
2025-06-19 23:02:22 +0900

    너무 졸려

--- C99_Step_by_Step/Step06/2444-별다이아.c
+++ C99_Step_by_Step/Step06/2444-별다이아.c
@@ -17,8 +17,40 @@
        ***
         *
 */
+void printSpace(int max, int n, int tf);
+void printStar(int max, int n);
+
+void printSpace(int max, int n, int tf) {
+    int i;
+    for (i = 0; i < n-1; i++) {
+        printf(" ");
+    }
+
+    if (tf == 1) {
+        printf("\n");
+        return;
+    }
+    printStar(max, i);
+
+}
+
+void printStar(int max, int n) {
+    int star = max - (n * 2);
+
+    int i;
+    for (i = 0; i < star; i++) {
+        printf("*"); 
+    }
+    printSpace(max, i, 1);
+}
 
 int main() {
-
+    int n;
+    scanf("%d", &n);
+    int max = n * 2 - 1;
+    for (int i = 0; i < n; i++) {
+        printSpace(max, n - i, 0);
+    }
+    // 일단 세모 완성.. 자야지...
     return 0;
 }
